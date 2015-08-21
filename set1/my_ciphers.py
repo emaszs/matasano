18ccd71d8ad812212aed62c6ae8def2cb21e2fdb
Emilis <phonixreal@gmail.com>
2015-08-21 10:44:20 +0200

    fixes

--- set1/my_ciphers.py
+++ set1/my_ciphers.py
@@ -1,3 +1,9 @@
+'''
+Created on Aug 21, 2015
+
+@author: EmilisR
+'''
+
 import base64
 import codecs
 
@@ -15,8 +21,3 @@ class my_ciphers:
             xoredStr += hex(m1 ^ m2)[2:]
         return xoredStr
 
-c = my_ciphers()
-a = c.fixedXOR("1c0111001f010100061a024b53535009181c", "686974207468652062756c6c277320657965")
-print (codecs.decode(a, "hex"))
-b = c.hexToBase64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
-print (codecs.decode("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d", "hex"))
